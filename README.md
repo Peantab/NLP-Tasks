f9dc660d2760c3525922b7674d4f064a0c1ed4ba
Paweł Taborowski <peantab@gmail.com>
2019-03-13 12:22:50 +0100

    Introduced README.md, refactored main.py, initial work for task 1.

---
+++ README.md
@@ -0,0 +1,11 @@
+# [NLP] regexps
+
+Task for Natural Language Processing course, focusing on the use of regular expressions.
+
+[Task description](./1-regexp.md).
+
+## Requirements
+I assume input files in subdirectory `ustawy`.
+
+Tested with:
+* regex==2019.3.12
--- main.py
+++ main.py
@@ -2,20 +2,39 @@ import os
 import regex
 
 
-def ustawaCounter():
+DIRECTORY = 'ustawy'
+
+
+def external_refs():
+    file = 'ustawy/1993_599.txt'
+    (year, pos) = year_and_position(file)
+    example = file_content(file)
+    print(year, ' ', pos)
+
+
+def ustawa_counter():
     counter = 0
     for file in generate_paths():
-        with open(file, 'r') as inp:
-            law = '\n'.join(inp.readlines())
-            ustawa = regex.findall(r'(?:\bustawa\b)|(?:\bustawy\b)|(?:\bustaw\b)|(?:\bustawie\b)|(?:\bustawom\b)|(?:\bustawę\b)|(?:\bustawą\b)|(?:\bustawami\b)|(?:\bustawach\b)|(?:\bustawo\b)|(?:(?<=[\b\.])U\.)', law, regex.IGNORECASE)
-            counter += len(ustawa)
+        law = file_content(file)
+        ustawa = regex.findall(r'(?:\bustawa\b)|(?:\bustawy\b)|(?:\bustaw\b)|(?:\bustawie\b)|(?:\bustawom\b)|(?:\bustawę\b)|(?:\bustawą\b)|(?:\bustawami\b)|(?:\bustawach\b)|(?:\bustawo\b)|(?:(?<=[\b\.])U\.)', law, regex.IGNORECASE)
+        counter += len(ustawa)
     print(counter)
 
 
 def generate_paths():
-    (directory, _, filenames) = next(os.walk('ustawy'))
-    return map(lambda name: os.path.join(directory, name), filenames)
+    (_, _, filenames) = next(os.walk(DIRECTORY))
+    return map(lambda name: os.path.join(DIRECTORY, name), filenames)
+
+
+def file_content(path):
+    with open(path, 'r') as inp:
+        return '\n'.join(inp.readlines())
+
+
+def year_and_position(path):
+    match = regex.search(DIRECTORY + os.path.sep + r'(?P<year>\d+)_(?P<pos>\d+)\.txt', path)
+    return match['year'], match['pos']
 
 
 if __name__ == '__main__':
-    ustawaCounter()
+    external_refs()
