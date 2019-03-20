6715f6b9844f0e993ab189047c8dc44746289b5d
Paweł Taborowski <peantab@gmail.com>
2019-03-20 21:39:21 +0100

    Loading data into ElasticSearch, initial implementation of task 1
    index configuration and query for task 1 yet to be verified.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import os
+import re
+import sys
+
 from elasticsearch import Elasticsearch
+
+DIRECTORY = 'ustawy'
 
 es = Elasticsearch()  # By default we connect to localhost:9200
 
 
-def main():
+def init():
     prepare_index()
-    remove_index()
+    load_data()
 
 
 def prepare_index():
@@ -61,5 +67,50 @@ def remove_index():
     print(es.indices.delete(index="law"))
 
 
+def load_data():
+    for file in generate_paths():
+        bill = file_content(file)
+        (year, pos) = year_and_position(file)
+        bill_id = (year % 100) * 10000 + pos
+        json = {"bill": bill}
+        es.create(index="law", doc_type="_doc", id=bill_id, body=json)
+
+
+def generate_paths():
+    (_, _, filenames) = next(os.walk(DIRECTORY))
+    return map(lambda name: os.path.join(DIRECTORY, name), filenames)
+
+
+def file_content(path):
+    with open(path, 'r') as inp:
+        return ''.join(inp.readlines())
+
+
+def year_and_position(path):
+    match = re.search(DIRECTORY + os.path.sep + r'(?P<year>\d+)_(?P<pos>\d+)\.txt', path)
+    return int(match['year']), int(match['pos'])
+
+
+def ustawa_counter():
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"bill": "ustawa"}},
+                ],
+                # "filter": [
+                #   { "range": { "publish_date": { "gte": "2015-01-01" }}}
+                # ]
+            }
+        }
+    }
+    print(es.search(index="law", doc_type="_doc", body=query))
+
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == 'stop':
+        remove_index()
+    elif len(sys.argv) > 1 and sys.argv[1] == 'init':
+        init()
+    else:
+        ustawa_counter()
